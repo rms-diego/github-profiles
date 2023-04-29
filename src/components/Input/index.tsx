d96da9794760712838e92f69e9e054d9ac1a4870
Diego Ramos <diegorbrito9@gmail.com>
2023-04-29 18:41:25 -0300

    feat: criando funcionalidade da 'home page'

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,17 +4,24 @@ type InputProps = {
   label: string;
   type?: string;
   placeholder: string;
+  handleInputChange: (payload: string) => void;
 };
 
 export default function Input({
   label,
   placeholder,
   type = "text",
+  handleInputChange,
 }: InputProps) {
   return (
     <label className={styles.inputContainer}>
       {label}
-      <input type={type} placeholder={placeholder} />
+
+      <input
+        type={type}
+        placeholder={placeholder}
+        onChange={({ target }) => handleInputChange(target.value)}
+      />
     </label>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,32 @@
 import Head from "next/head";
+import { FormEvent, useEffect } from "react";
 
 import styles from "@/styles/pages/home/styles.module.scss";
 import Input from "@/components/Input";
 import Footer from "@/components/Footer";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { push } = useRouter();
+
+  const [githubUserName, setGithubUserName] = useState<string>("");
+
+  const handleInputChange = (payload: string) => {
+    setGithubUserName(payload);
+  };
+
+  const handleSearchUser = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (!githubUserName) {
+      return alert("Is missing github username");
+    }
+
+    setGithubUserName((prevState) => prevState.trim());
+    push(`/profile/${githubUserName}`);
+  };
+
   return (
     <>
       <Head>
@@ -14,8 +36,12 @@ export default function Home() {
       <main className={styles.mainContainer}>
         <h1>Github Profiles</h1>
 
-        <form action="" className={styles.formContainer}>
-          <Input label="Username github" placeholder="john-doe" />
+        <form onSubmit={handleSearchUser} className={styles.formContainer}>
+          <Input
+            label="Username github"
+            placeholder="john-doe"
+            handleInputChange={handleInputChange}
+          />
 
           <button type="submit">Search</button>
         </form>
